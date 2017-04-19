0be1d1b3f720974abea65e5857087220938a536c
ltlam <larry.t.lam@ucla.edu>
2017-04-19 08:29:14 -0700

    Fix error with underscores within chromosome label
    Traceback (most recent call last):
      File "/u/home/c/chantle/bin/BSseeker2/scripts/pos_to_frag.py", line 119, in <module>
        lpos = load_pos_fcgmap(args.fcgmap)
      File "/u/home/c/chantle/bin/BSseeker2/scripts/pos_to_frag.py", line 61, in load_pos_fcgmap
        str_chr, str_pos = lcols[0].split('_')
    ValueError: too many values to unpack

--- scripts/pos_to_frag.py
+++ scripts/pos_to_frag.py
@@ -23,33 +23,32 @@ def test_bbox():
         print '%d'%lval[0]
     '''
 
-def chrlbl_to_num(str_chr):
-    str_chr = str_chr.lstrip('chr')
-    if str_chr.isalpha():
-        n_chr = ord(str_chr)
+def chrlbl_to_num(str_chr,l_chr_lbl):
+    n_chr_id = 0
+    if str_chr in l_chr_lbl:
+        n_chr_id = l_chr_lbl.index(str_chr)
     else:
-        n_chr = int(str_chr)
-    return n_chr
+        l_chr_lbl.append(str_chr)
+        n_chr_id = len(l_chr_lbl)
+    return n_chr_id
 
-def num_to_chrlbl(nnum):
-    if nnum < min([ord('M'),ord('X'),ord('Y')]):
-        str_chr = 'chr%d'%nnum
-    else:
-        str_chr = 'chr%s'%chr(nnum)
-    return str_chr
+def num_to_chrlbl(nnum,l_chr_lbl):
+    if nnum >= len(l_chr_lbl):
+        print 'chr label index out of range %d'%nnum
+    return l_chr_lbl[nnum]
 
-def load_mappable(str_mappable_file):
+def load_mappable(str_mappable_file,l_chr_lbl):
     frag_idx = index.Index()
     for str_line in open(str_mappable_file,'r'):
         lcols = str_line.rstrip().split('\t')
-        n_chr = chrlbl_to_num(lcols[0])
+        n_chr = chrlbl_to_num(lcols[0],l_chr_lbl)
         n_frag_id = int(lcols[1])
         n_start = int(lcols[2])+1
         n_end = int(lcols[3])+1
         frag_idx.insert(n_frag_id,(n_start,n_chr,n_end,n_chr))
     return frag_idx
 
-def load_pos_fcgmap(str_comm_cgmap):
+def load_pos_fcgmap(str_comm_cgmap,l_chr_lbl):
     l_pos = []
     fin = open(str_comm_cgmap,'r')
     b_is_header = True
@@ -61,31 +60,32 @@ def load_pos_fcgmap(str_comm_cgmap):
         l_chr_lbls = lcols[0].split('_')
         str_chr = '_'.join(l_chr_lbls[:-1])
         str_pos = l_chr_lbls[-1]
-        n_chr = chrlbl_to_num(str_chr)
+        n_chr = chrlbl_to_num(str_chr,l_chr_lbl)
         n_pos = int(str_pos)
         l_pos.append((n_pos,n_chr,n_pos,n_chr))
     fin.close()
     return l_pos
 
-def load_comm_pos(str_pos):
+def load_comm_pos(str_pos,l_chr_lbl):
     l_pos = []
     for str_line in open(str_pos):
         lcols = str_line.rstrip().split(' ')
-        n_chr = chrlbl_to_num(lcols[0])
+        n_chr = chrlbl_to_num(lcols[0],l_chr_lbl)
         n_pos = int(lcols[1])
         l_pos.append((n_pos,n_chr,n_pos,n_chr))
     return l_pos
 
-def get_pos_to_frag(frag_idx,lpos):
+def get_pos_to_frag(frag_idx,lpos,l_chr_lbl):
     d_pos_to_frag = {}
     for i in range(len(lpos)):
         l_cur_pos = lpos[i]
 
-        str_cur_pos = '%s_%d'%(num_to_chrlbl(l_cur_pos[1]),l_cur_pos[0])
+        str_cur_pos = '%s_%d'%(num_to_chrlbl(l_cur_pos[1],l_chr_lbl),l_cur_pos[0])
         l_hits = frag_idx.intersection(l_cur_pos,objects=True)
         for i in l_hits:
             nstart,nchr,nend,nchr2 = i.bbox
-            str_frag = '%s:%d-%d'%(num_to_chrlbl(l_cur_pos[1]),int(nstart),int(nend))
+            str_frag = '%s:%d-%d'%(num_to_chrlbl(l_cur_pos[1],l_chr_lbl),int(nstart),
+                                   int(nend))
             d_pos_to_frag[str_cur_pos] = str_frag
             break
     return d_pos_to_frag
@@ -113,16 +113,19 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--fcgmap', type=str, help="sample methylation matrix")
     args = parser.parse_args()
 
+    #list of chr labels
+    l_chr_lbls = []
+
     #load fragments into rtree
-    frag_idx = load_mappable(args.mappable)
+    frag_idx = load_mappable(args.mappable,l_chr_lbls)
 
     lpos = []
     if args.pos == '':
-        lpos = load_pos_fcgmap(args.fcgmap)
+        lpos = load_pos_fcgmap(args.fcgmap,l_chr_lbls)
     else:
         lpos = load_comm_pos(args.pos)
 
-    d_pos_to_frag = get_pos_to_frag(frag_idx,lpos)
+    d_pos_to_frag = get_pos_to_frag(frag_idx,lpos,l_chr_lbls)
 
     proc_fcgmap(d_pos_to_frag,args.fcgmap,re.sub('.txt$','_frag.txt',args.fcgmap))
 
